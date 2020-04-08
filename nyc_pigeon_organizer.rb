72b6c83a79d87f6f3332d97c0aa74e9fccca6b06
tiya adugna <tiyakefalee@gmail.com>
2020-04-08 07:24:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,9 @@ def nyc_pigeon_organizer(data)
        if !hash[name].has_key?(key)
          has_key[name][key] = {}
        end
-       if 
+       if !hash[name][key].has_key?(key)
+         has_key[name][key] = {}
+       end 
 end 
 end
 end
