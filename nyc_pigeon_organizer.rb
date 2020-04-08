be68841a99f9ae58b775003b8eb95446bf5670f4
tiya adugna <tiyakefalee@gmail.com>
2020-04-08 07:35:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+  
+  data.each do |key, h|
+  
+    h.each do |k, arr|
+    
+      arr.each do |name|
+        if !hash.has_key?(name)
+        hash[name] = {}
+       end 
+       if !hash[name].has_key?(key)
+         hash[name][key] = []
+       end
+       if !hash[name][key].include?(k)
+         hash[name][key] << k.to_s 
+       end 
+end 
+end
+end
+ hash
 end
