c32edcd5562418d51e159b01633f945244c8b3fe
tiya adugna <tiyakefalee@gmail.com>
2020-04-08 07:23:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,9 +7,9 @@ def nyc_pigeon_organizer(data)
     h.each do |k, arr|
     
       arr.each do |name|
-        if !hash[name].has_key?(key)
+        if !hash.has_key?(name)
         hash[name] = {}
-       
+       end 
        if !hash[name].has_key?(key)
          has_key[name][key] = {}
        end
