adfa44f41568ed675f740564db64b0a6483ae018
tiya adugna <tiyakefalee@gmail.com>
2020-04-08 07:09:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  arr = []
+  count = 0 
+  data.map do |key, value|
+    
+    a = data[key][value]
+    
+    b = a[key][value]
+    if !arr.include?(b[count]) 
+    arr.push(b[count])
+  end
+end 
+ hash
 end
